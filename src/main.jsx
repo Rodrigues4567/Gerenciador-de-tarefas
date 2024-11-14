deedd46cc1b065c205d35bbfabef415f74116f30
Allyson Rodrigues <125301945+Rodrigues4567@users.noreply.github.com>
2024-11-14 16:53:19 -0300

    page DescriptionPage created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import DescriptionPage from './pages/description-page/main.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/description",
+    element: <DescriptionPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/description-page/main.jsx
@@ -0,0 +1,10 @@
+
+function DescriptionPage() {
+    return (
+        <div>
+            <h1>Pagina da descrição da tarefa</h1>
+        </div>
+    )
+}
+
+export default DescriptionPage
